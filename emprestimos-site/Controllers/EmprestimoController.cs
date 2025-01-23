bc1c751516204e0c587dcbb4267d229742d9b109
Igor02b1 <igor.lsilva@al.infnet.edu.br>
2025-01-23 00:55:12 -0300

    Fazendo a leitura dos dados que estão no banco de dados.

---
+++ emprestimos-site/Controllers/EmprestimoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using emprestimos_site.Data;
+using emprestimos_site.Models;
+
+namespace emprestimos_site.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Emprestimo()
+        {
+            IEnumerable<EmprestimoModel> emprestimos = _db.Emprestimo;
+            return View(emprestimos);
+        }
+    }
+}
